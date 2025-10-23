8d0be41cdb51020bec9beb3527bf0867ef9f8fa5
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-10-23 16:38:06 +0530

    created in app slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css'
+import { decrement, increment } from './Features/Counter/CounterSlice';
 
 function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+     dispatch(increment())
+  }
+
+  function handleDecrement() {
+    dispatch(decrement())
+  }
 
   return (
-    <>
+    <div className='container'>
       <h2>learn with react toolkit</h2>
-    </>
+      
+      <button onClick={handleIncrement}>+</button>
+      <h2>Count : {count} </h2>
+      <button onClick={handleDecrement}>-</button>
+    </div>
   )
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
+import { counterSlice } from '../Features/Counter/CounterSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterSlice,
+    },
 });
